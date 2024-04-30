f8b3cf706d5c4a7534c5145f11d53fb598e8ced2
Mark Petrov <drafwodgaming@gmail.com>
2024-04-30 15:03:55 +0300

    update: error

--- source/handlers/errorsHandler.js
+++ source/handlers/errorsHandler.js
@@ -1,15 +1,18 @@
 const { Client } = require('discord.js');
 const en = require('@config/languages/en.json');
 const kleur = require('kleur');
+
 /**
  * @param {Client} client
  */
 module.exports = async client => {
-	process.on('unhandledRejection', error => {
-		console.log(kleur.red(en.logs.unhandledRejection), `${error}`);
+	process.on('unhandledRejection', (reason, promise) => {
+		console.error(kleur.red(en.logs.unhandledRejection));
+		console.error(reason);
 	});
 
 	process.on('uncaughtException', error => {
-		console.log(kleur.red(en.logs.uncaughtException), `${error}`);
+		console.error(kleur.red(en.logs.uncaughtException));
+		console.error(error);
 	});
 };
